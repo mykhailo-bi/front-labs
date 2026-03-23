e0dd64b1345ca1c0b61f080e70be864af30fcaf6
Mykhailo B <mykhailo.bilousov.shi.2024@lpnu.ua>
2026-03-23 22:42:53 +0200

    build: update server port to 3000

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     },
     server: {
         host: '0.0.0.0',
-        port: 8000,
+        port: 3000,
         proxy: {
             '/api': 'http://localhost:8000',
         },
