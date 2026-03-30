daf6779e4a8022e027383cd52901af277a78c786
Mykhailo B <mykhailo.bilousov.shi.2024@lpnu.ua>
2026-03-30 23:10:44 +0300

    chore(frontend): proxy media requests to backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,6 +19,10 @@ export default defineConfig({
                 target: 'http://127.0.0.1:9000',
                 changeOrigin: true,
             },
+            '/media': {
+                target: 'http://127.0.0.1:9000',
+                changeOrigin: true,
+            },
         },
     },
     resolve: {
